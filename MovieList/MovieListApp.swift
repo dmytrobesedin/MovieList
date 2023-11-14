02846601217d160187a1f82d06f20a125bb5db1c
Dmytro <dmytro.besedin@gmail.com>
2023-11-14 21:51:29 +0200

    Initial Commit

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Dmytro Besedin on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
